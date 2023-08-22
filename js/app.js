287f43c21a57be36088f37b76d061596306a4aec
Shihab-283 <135458998+Shihab-283@users.noreply.github.com>
2023-08-22 23:00:46 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// document.getElementById('nav').addEventListener('click', function() {
+//     document.getElementById('nav').src = './global warming.jpeg'
+// });
+
+
+
+// HW-1-Start
+
+document.getElementById('img').addEventListener('click', function() {
+document.getElementById('img').src = './global warming.jpeg'
+});
+
+// HW-1-End
+
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+
+
+
